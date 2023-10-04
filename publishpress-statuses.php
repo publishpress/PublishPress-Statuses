e37a1e4060c5286b10c25f3a8392ca7d9b713778
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2023-10-03 22:12:44 -0400

    Move Module_Base, ModuleAdminUI_Base out of lib/vendor

--- publishpress-statuses.php
+++ publishpress-statuses.php
@@ -138,9 +138,9 @@ if (!defined('PUBLISHPRESS_STATUSES_VERSION')) {
         define('PUBLISHPRESS_STATUSES_URL', trailingslashit(plugins_url('', __FILE__)));
         define('PUBLISHPRESS_STATUSES_DIR', __DIR__);
 
-        require_once(__DIR__ . '/lib/vendor/publishpress/PublishPress_Functions.php');
+        require_once(__DIR__ . '/lib/PublishPress_Functions.php');
 
-        require_once(__DIR__ . '/lib/vendor/publishpress/publishpress-module/Module_Base.php');
+        require_once(__DIR__ . '/lib/publishpress-module/Module_Base.php');
         new \PublishPress\PPP_Module_Base();
 
         require_once(__DIR__ . '/LibInstanceProtection.php');
