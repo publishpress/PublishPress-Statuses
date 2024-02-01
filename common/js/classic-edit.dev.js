ef3a4d1314c82cfb025abc369a792ff0a258abfa
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2024-01-31 23:08:51 -0500

    ACF: Update attempt with missing required fields left Classic Publish / Update button hidden

--- common/js/classic-edit.dev.js
+++ common/js/classic-edit.dev.js
@@ -64,8 +64,14 @@ jQuery(document).ready(function ($) {
 
     $('#save-post, #publish').click(function () {
         setTimeout(() => {
-            $('#save-post').hide();
-            $('#publish').hide();
+            $('#save-post').prop('disabled', true);
+            $('#publish').prop('disabled', true);
+        }, 100);
+
+        setInterval(function() {
+            if ($('#publish').val() != ppObjEdit.publish) {
+                $('#publish').val(ppObjEdit.publish);
+            }
         }, 100);
     });
 
