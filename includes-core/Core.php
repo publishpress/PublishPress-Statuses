728774cac1b50582aee91b3c4149f1c5021fb360
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2025-03-05 15:51:15 -0500

    plugin review prompt: fix confirmation, add missing logo

--- includes-core/Core.php
+++ includes-core/Core.php
@@ -8,7 +8,7 @@ class Core {
         if (! $publishpress_statuses_loaded_by_pro) {
             add_action('init', function() { // late execution avoids clash with autoloaders in other plugins
                 if (\PublishPress_Statuses::isPluginPage()
-                    || (defined('DOING_AJAX') && DOING_AJAX && !empty($_REQUEST['action']) && (false !== strpos(sanitize_key($_REQUEST['action']), 'press-permit-core')))  // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+                    || (defined('DOING_AJAX') && DOING_AJAX && !empty($_REQUEST['action']) && (false !== strpos(sanitize_key($_REQUEST['action']), 'publishpress-statuses')))  // phpcs:ignore WordPress.Security.NonceVerification.Recommended
                 ) {
                     if (!class_exists('\PublishPress\WordPressReviews\ReviewsController')) {
                         include_once PUBLISHPRESS_STATUSES_ABSPATH . '/lib/vendor/publishpress/wordpress-reviews/ReviewsController.php';
@@ -18,7 +18,7 @@ class Core {
                         $reviews = new \PublishPress\WordPressReviews\ReviewsController(
                             'publishpress-statuses',
                             'PublishPress Statuses',
-                            plugin_dir_url(PUBLISHPRESS_STATUSES_FILE) . 'common/img/permissions-wp-logo.jpg'
+                            plugin_dir_url(PUBLISHPRESS_STATUSES_FILE) . 'common/img/statuses-wp-logo.png'
                         );
 
                         add_filter('publishpress_wp_reviews_display_banner_publishpress-statuses', [$this, 'shouldDisplayBanner']);
